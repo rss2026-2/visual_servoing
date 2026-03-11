287acc1218e94cdd998cae0292f7650676974bfb
DwnNyxDev <vatelvandell@gmail.com>
2026-03-11 17:37:03 -0400

    Fixed bug with reading cone template

--- visual_servoing/visual_servoing/cone_detector.py
+++ visual_servoing/visual_servoing/cone_detector.py
@@ -50,7 +50,8 @@ class ConeDetector(Node):
         #################################
 
         image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
-        bbox = cd_color_segmentation(image, cv2.imread("./visual_servoing/computer_vision/test_images_cone/cone_template.png"))
+        cone_template = cv2.imread("./computer_vision/test_images_cone/cone_template.png")
+        bbox = cd_color_segmentation(image, cone_template)
         cv2.rectangle(image, bbox[0], bbox[1], (255,0,0), 2)
 
         debug_msg = self.bridge.cv2_to_imgmsg(image, "bgr8")
