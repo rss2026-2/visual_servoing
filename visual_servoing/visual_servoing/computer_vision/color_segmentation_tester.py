2c62faf2ace3497246d3398cd5f615e0284c67e0
DwnNyxDev <vatelvandell@gmail.com>
2026-03-14 14:56:42 -0400

    Just some trial information

--- visual_servoing/visual_servoing/computer_vision/color_segmentation_tester.py
+++ visual_servoing/visual_servoing/computer_vision/color_segmentation_tester.py
@@ -38,7 +38,7 @@ def run_trial_random(num_iterations, trial_name):
         current_time = datetime.now().strftime("%I:%M:%S %p %h %d, %Y")
         file.write("[ "+trial_name+"] ("+current_time+")\nTotal Iterations: "+str(num_iterations)+"\n\n")
 
-    columns_lis = ["Iteration #", "Avg", "Min", "Avg_Min_Mean"]
+    columns_lis = ["Iteration #", "Avg", "Min", "Target"]
     pd.DataFrame(columns = columns_lis).to_csv(csv_path, mode="a",index=False)
 
     trial_data = { #stores best data of current trial
@@ -46,7 +46,7 @@ def run_trial_random(num_iterations, trial_name):
         "filter_specs": None,
         "avg": 0,
         "min": 0,
-        "avg_min_mean": 0,
+        "target": 0,
         "params": None
     }
 
@@ -63,24 +63,24 @@ def run_trial_random(num_iterations, trial_name):
         if new_score: #if the distance range didn't fail on one of the tests
             new_avg, new_min = new_score["avg"], new_score["min"]
 
-            new_avg_min_mean = (new_avg + new_min) / 2
+            new_target = (new_avg + new_min) / 2
 
-            if new_avg_min_mean > trial_data["avg_min_mean"]:
+            if new_target > trial_data["target"]:
             # if new_avg > trial_data["avg"]:
                 trial_data["range"] = new_distance_range
                 trial_data["filter_specs"] = new_filter_specs
                 trial_data["avg"] = new_avg
                 trial_data["min"] = new_min
-                trial_data["avg_min_mean"] = new_avg_min_mean
+                trial_data["target"] = new_target
                 trial_data["params"] = (new_distance_range, new_filter_specs)
                 
 
                 with open(txt_path, "a") as file:
                     file.write(create_trial_record(iter = i, data = trial_data))
 
-                pd.DataFrame([[i, trial_data["avg"], trial_data["min"], trial_data["avg_min_mean"]]], columns = columns_lis).to_csv(csv_path, mode="a", index=False, header = False)
+                pd.DataFrame([[i, trial_data["avg"], trial_data["min"], trial_data["target"]]], columns = columns_lis).to_csv(csv_path, mode="a", index=False, header = False)
     
-    return f"Avg: {trial_data["avg"]}\nMin: {trial_data["min"]}\nAvg_Min_Mean: {trial_data["avg_min_mean"]}\nParams: {trial_data["params"]}"
+    return f"Avg: {trial_data["avg"]}\nMin: {trial_data["min"]}\nTarget: {trial_data["target"]}\nParams: {trial_data["params"]}"
 
 
 def bayesian_func(hue_low, hue_high, sat_low, sat_high, val_low, val_high, 
@@ -103,13 +103,13 @@ def bayesian_func(hue_low, hue_high, sat_low, sat_high, val_low, val_high,
 
     
 
-def run_trial_bayesian(num_iterations, trial_name):
+def run_trial_bayesian(num_iterations, trial_name, starting_point = None):
     trial_data = { #stores best data of current trial
         "range": None,
         "filter_specs": None,
         "avg": 0,
         "min": 0,
-        "avg_min_mean": 0,
+        "target": 0,
         "params": None
     }
 
@@ -123,24 +123,25 @@ def run_trial_bayesian(num_iterations, trial_name):
     }
 
     max_filters = 6
+    switch_bounds = (0.0, 1.999)
     size_bounds = (2.0, 8.0)
-    iter_bounds = (1.0, 6.0)
+    iter_bounds = (1.0, 4.0)
 
-
-    # for i in range(1, max_filters+1):
-    #     if i == 2:
-    #         param_bounds[f"switch_{i}"] = (1,1.25)
-    #         param_bounds[f"size_{i}"] = (4.0,4.25)
-    #         param_bounds[f"iter_{i}"] = (1.0,2.99)
-    #     else:
-    #         param_bounds[f"switch_{i}"] = (0,0.5)
-    #         param_bounds[f"size_{i}"] = size_bounds
-    #         param_bounds[f"iter_{i}"] = iter_bounds
 
     for i in range(1, max_filters+1):
-        param_bounds[f"switch_{i}"] = (1,2.999)
-        param_bounds[f"size_{i}"] = size_bounds
-        param_bounds[f"iter_{i}"] = iter_bounds
+        if i == 2: #only turn on the second switch, so only use one dilation filter with size 5 and 2 iterations
+            param_bounds[f"switch_{i}"] = (1,1.99)
+            param_bounds[f"size_{i}"] = (5.0,5.99)
+            param_bounds[f"iter_{i}"] = (2.0,2.99)
+        else:
+            param_bounds[f"switch_{i}"] = (0,0.99)
+            param_bounds[f"size_{i}"] = size_bounds
+            param_bounds[f"iter_{i}"] = iter_bounds
+
+    # for i in range(1, max_filters+1):
+    #     param_bounds[f"switch_{i}"] = switch_bounds
+    #     param_bounds[f"size_{i}"] = size_bounds
+    #     param_bounds[f"iter_{i}"] = iter_bounds
 
     # acquisition_function = acquisition.ExpectedImprovement(xi=0.0)
     optimizer = BayesianOptimization(
@@ -160,38 +161,36 @@ def run_trial_bayesian(num_iterations, trial_name):
         current_time = datetime.now().strftime("%I:%M:%S %p %h %d, %Y")
         file.write("[ "+trial_name+ "] ("+current_time+")\nTotal Iterations: "+str(num_iterations)+"\n\n")
 
-    columns_lis = ["Iteration #", "Avg", "Min", "Avg_Min_Mean"]
+    columns_lis = ["Iteration #", "Avg", "Min", "Target"]
     pd.DataFrame(columns = columns_lis).to_csv(csv_path, mode="a",index=False)
 
-    # | 12        | 0.8903852 | 0.8216992 | 1.0       | 0.1553830 | 1.0       | 0.4302919 | 1.0       | 0.1272110 | 4.5022429 | 2.1376954 | 1.6276444 | 3.1547204 | 1.4919597 | 0.5449078 | 6.3001111 | 1.1509766 | 0.9768408 | 4.0874484 | 2.7310682 | 0.6673591 | 4.6589857 | 4.2833414 | 0.1877486 | 6.1806158 | 1.4588511 |
-    # starting_point = {'hue_low': np.float64(0.26708526688793505), 'hue_high': np.float64(0.8421780462961072), 'sat_low': np.float64(0.15168815426382168), 'sat_high': np.float64(1.0), 'val_low': np.float64(0.4450195301618705), 'val_high': np.float64(1.0), 'switch_1': np.float64(0.19736675015654012), 'size_1': np.float64(5.401711360055775), 'iter_1': np.float64(1.5017263013699773), 'switch_2': np.float64(1.6778872383401953), 'size_2': np.float64(4.490137140266256), 'iter_2': np.float64(1.0927518185930578), 'switch_3': np.float64(0.8924516233867578), 'size_3': np.float64(5.81425090360953), 'iter_3': np.float64(1.0), 'switch_4': np.float64(0.8808059906878062), 'size_4': np.float64(3.939185464183444), 'iter_4': np.float64(2.0818692891735138), 'switch_5': np.float64(0.7167230310191588), 'size_5': np.float64(4.430389205765636), 'iter_5': np.float64(4.26393011668932), 'switch_6': np.float64(0.8881518410658341), 'size_6': np.float64(5.459211480331205), 'iter_6': np.float64(2.385359317018129)}
-    # starting_target = 0.8936774856127421
-
-    # optimizer.register(params = starting_point, target = starting_target)
+    if starting_point is not None:
+        starting_params, starting_target = starting_point
+        optimizer.register(params = starting_params, target = starting_target)
 
     for i in range(num_iterations):
         new_params = optimizer.suggest()
         new_results = bayesian_func(**new_params)
         new_avg, new_min = new_results["score"]["avg"], new_results["score"]["min"]
 
-        new_avg_min_mean = (new_avg + new_min) / 2
+        new_target = (new_avg + new_min) / 2 - np.sqrt(np.abs(new_avg - new_min))
 
-        optimizer.register(params = new_params, target = new_avg_min_mean)
+        optimizer.register(params = new_params, target = new_target)
 
-        if new_avg_min_mean > trial_data["avg_min_mean"]: #if there is a net increase
+        if new_target > trial_data["target"]: #if there is a net increase
             trial_data["range"] = np.array(new_results["distance_range"]).tolist()
             trial_data["filter_specs"]= np.array(new_results["filter_specs"]).tolist()
             trial_data["avg"]= new_avg
             trial_data["min"] = new_min
-            trial_data["avg_min_mean"] = new_avg_min_mean
+            trial_data["target"] = new_target
             trial_data["params"] = new_params
 
             with open(txt_path, "a") as file:
                 file.write(create_trial_record(iter = i, data = trial_data))
 
-            pd.DataFrame([[i, trial_data["avg"], trial_data["min"], trial_data["avg_min_mean"]]], columns = columns_lis).to_csv(csv_path, mode="a", index=False, header = False)
+            pd.DataFrame([[i, trial_data["avg"], trial_data["min"], trial_data["target"]]], columns = columns_lis).to_csv(csv_path, mode="a", index=False, header = False)
 
-    return f"Avg: {trial_data["avg"]}\nMin: {trial_data["min"]}\nAvg_Min_Mean: {trial_data["avg_min_mean"]}\nParams: {trial_data["params"]}"
+    return f"Avg: {trial_data["avg"]}\nMin: {trial_data["min"]}\nTarget: {trial_data["target"]}\nParams: {trial_data["params"]}"
 
 def create_trial_record(iter, data):
     record_msg = f"""Iteration {iter+1}:
@@ -201,8 +200,9 @@ def create_trial_record(iter, data):
         Value: {data["range"][2]}
     Avg: {data["avg"]}
     Min: {data["min"]}
-    Avg_Min_Mean: {data["avg_min_mean"]}
+    Target: {data["target"]}
     Filter Specs: {data["filter_specs"]}
+    Params: {data["params"]}
 
 """
     return record_msg
@@ -211,10 +211,10 @@ def plot_trial(trial_name, show = True, save = True):
     trial_csv_path = trials_directory+"/"+trial_name+"/"+trial_name+".csv"
     df = pd.read_csv(trial_csv_path)
 
-    ax = df.plot(kind="line", x="Iteration #", y=["Avg", "Min", "Avg_Min_Mean"], title=trial_name, ylim=(0.0,1.0))
+    ax = df.plot(kind="line", x="Iteration #", y=["Avg", "Min", "Target"], title=trial_name, ylim=(0.0,1.0))
     ax.scatter(df["Iteration #"], df["Avg"])
     ax.scatter(df["Iteration #"], df["Min"])
-    ax.scatter(df["Iteration #"], df["Avg_Min_Mean"])
+    ax.scatter(df["Iteration #"], df["Target"])
 
     if save:
         trial_save_path = trials_directory+"/"+trial_name+"/"+trial_name+".png"
@@ -223,27 +223,28 @@ def plot_trial(trial_name, show = True, save = True):
     if show:
         plt.show()
 
-def run_trials(num_trials, num_iterations, trial_name, trial_type = "random", plot = True):
+def run_trials(num_trials, num_iterations, trial_name, starting_point = None, trial_type = "random", plot = True):
     print("Trials Started")
     trial_name = trial_type+"_"+trial_name+"_trial"
     for i in range(1, num_trials+1):
-
+        new_trial_name = trial_name+"_"+str(i)
         if trial_type == "random":
-            print(run_trial_random(num_iterations, trial_name+"_"+str(i)))
+            print(run_trial_random(num_iterations, new_trial_name, starting_point))
         elif trial_type == "bayesian":
-            print(run_trial_bayesian(num_iterations, trial_name+"_"+str(i)))
+            print(run_trial_bayesian(num_iterations, new_trial_name, starting_point))
         
         if plot:
-            plot_trial(trial_name, show = False)
+            plot_trial(new_trial_name, show = False)
 
         print("Trials ",i," / ", num_trials," Completed")
 
 if __name__ == '__main__':
-    # run_trials(
-    #     num_trials = 6,
-    #     num_iterations = 300,
-    #     trial_type = "bayesian"
-    # )
 
-    run_trials(3, 100, "worm", "bayesian", True)
+    starting_params = {'hue_low': np.float64(0.21477133663277984), 'hue_high': np.float64(0.37984970977401367), 'sat_low': np.float64(0.09241312163553674), 'sat_high': np.float64(1.0), 'val_low': np.float64(0.49574286132606293), 'val_high': np.float64(1.0), 'switch_1': np.float64(0.5476381089856193), 'size_1': np.float64(7.52196191581993), 'iter_1': np.float64(1.3343218405685542), 'switch_2': np.float64(1.1804524546732253), 'size_2': np.float64(5.647604366663166), 'iter_2': np.float64(2.5681655681156053), 'switch_3': np.float64(0.2225009798771307), 'size_3': np.float64(7.702408967697437), 'iter_3': np.float64(2.2852301879515955), 'switch_4': np.float64(0.10410896739792515), 'size_4': np.float64(2.3929345185653026), 'iter_4': np.float64(3.779890429321243), 'switch_5': np.float64(0.9836820008836007), 'size_5': np.float64(4.907251392010587), 'iter_5': np.float64(2.197488266831934), 'switch_6': np.float64(0.39182893108109473), 'size_6': np.float64(7.068490808754028), 'iter_6': np.float64(2.0803699999669116)}
+    starting_target = 0.6203987132828895
+
+    starting_point = (starting_params, starting_target)
+
+    run_trials(10, 200, "rhino", starting_point = starting_point, trial_type = "bayesian", plot = True)
+
     pass
